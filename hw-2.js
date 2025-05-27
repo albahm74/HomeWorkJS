2810c7af34d38e7d86abeebdfd6bddcfc8d83eb5
Albert Akhmetov <albahm74@gmail.com>
2025-05-27 22:16:00 +0300

    Made hw-2

---
+++ hw-2.js
@@ -0,0 +1,75 @@
+//task_1__
+let num = 10;
+alert(num);
+num = 20;
+alert(num);
+
+//task_2__
+const releaseYearFirstIphone = 2007;
+alert(`Год выпуска первого Iphone: ${releaseYearFirstIphone}`);
+
+//task_3__
+const nameCreatorJavaScript = "Брендан Эйх";
+alert(`Имя Создателя JavaScript: ${nameCreatorJavaScript}`);
+
+//task_4__
+let a = 10;
+let b = 2;
+alert(`Числа: ${a}; ${b}.\nСумма: ${a + b}\nРазность: ${a - b}\nПроизведение: ${a * b}\nЧастное: ${a / b}\n`);
+
+//task_5__
+let result = 2;
+result**= 5;
+alert(result);
+
+//task_6__
+let c = 9;
+let d = 2;
+alert(`Остаток от деления: ${c % d}`);
+
+//task_7__
+let numb = 1;
+numb += 5;
+numb -= 3;
+numb *= 7;
+numb /= 3;
+numb ++;
+numb --;
+alert(numb);
+
+//task_8__
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+//task_9__
+let user = {
+    name: 'Klay',
+    age: 50,
+    isAdmin: true,
+};
+
+//task_10__
+let userName = prompt("Как Ваше имя?");
+alert(`Привет ${userName}!`);
+
+//additional__task_1_
+let number = prompt("Загадай число");
+
+//additional__task_2_
+let e = number*2;
+alert(e);
+
+//additional__task_3_
+let f = e + 10;
+alert(f);
+
+//additional__task_4_
+let g = f / 2;
+alert(g);
+
+//additional__task_5_
+let h = g - number;
+alert(h);
+
+//additional__task_6_
+alert(`Ответ равен ${h}`);
